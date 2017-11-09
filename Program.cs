134a75217e24ecb7c673839d29b4888fe48638f6
ThiagoCenturion <thiagocenturion@me.com>
2017-11-09 03:26:55 -0200

    * RSACriptUnip.cs: * CriptografiaRSA.csproj:
    * Program.cs: Adição da classe de criptografia e implementação dos
      códigos de cifragem. Falta verificar porque não está dando certo.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,403 @@ namespace CriptografiaRSA
 {
     class MainClass
     {
+        // Constantes
+        static string TEXTO_ENTRADA_INVALIDA = "Entrada inválida. Favor digitar uma das opções corretamente.\n";
+        static string TEXTO_ERRO_RETORNA_PRINCIPAL = "Erro ao tentar converter o número. Ele deve ser do tipo inteiro e maior que 0. O programa está retornando para a página principal.\n";
+        static string TEXTO_MAIOR_QUE_128_RETORNA_PRINCIPAL = "Texto digitado incorretamente. A quantidade de caracteres máxima é de 128. O programa está retornando para a página principal.\n";
+        static string TEXTO_ERRO_NUMERO_NAO_PRIMO_RETORNA_PRINCIPAL = "O(s) número(s) digitado(s) não é(são) primo(s). O Programa está retornando para a página principal.\n";
+
+        public struct Usuario
+        {
+            // Usuário e senha de acesso
+            public string login;
+            public string senha;
+        }
+
+        // Cria o struct do usuario
+        public static Usuario usuarioRoot;
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool sair = false;
+            int valorDigitado = 0;
+            string login = "";
+            string senha = "";
+            RSACriptUnip criptografer = null;
+
+            // Seta o usuario e senha de administrador
+            usuarioRoot.login = "criptografiarsa";
+            usuarioRoot.senha = "unip2017";
+
+            // Instancia o objeto de criptografia
+            criptografer = new RSACriptUnip();
+
+            while (sair == false)
+            {
+                // Solicita o usuario e senha
+                Console.WriteLine("\n=====================================");
+                Console.WriteLine("========== CRIPTOGRAFIA RSA =========");
+                Console.WriteLine("=====================================");
+                Console.Write("== USUÁRIO: ");
+                login = Console.ReadLine();
+                Console.Write("== SENHA: ");
+                senha = Console.ReadLine();
+                Console.WriteLine("=====================================");
+                Console.WriteLine("");
+
+                // Verifica se o login e senha digitados existem
+                if (ValidaUsuario(login, senha))
+                {
+                    // Se não validou, refaz o looping
+                    sair = true;
+                }
+            }
+
+            sair = false;
+
+            while (sair == false)
+            {
+                // Imprime os controles de decisões
+                Console.WriteLine("\n=====================================");
+                Console.WriteLine("========== ACESSO PERMITIDO =========");
+                Console.WriteLine("=====================================");
+                Console.WriteLine("= Digite \"1\" para GERAR AS CHAVES   =");
+                Console.WriteLine("= Digite \"2\" para CRIPTOGRAFAR      =");
+                Console.WriteLine("= Digite \"3\" para DESCRIPTOGRAFAR   =");
+                Console.WriteLine("= Digite \"4\" para SAIR              =");
+                Console.WriteLine("=====================================");
+
+                // Tenta pegar o valor digitado
+                if (int.TryParse(Console.ReadLine(), out valorDigitado) == false)
+                {
+                    // Caso tenha ocorrido um erro ao tentar converter o valor para inteiro, exibe uma mensagem de erro
+                    Console.WriteLine(TEXTO_ENTRADA_INVALIDA);
+                    continue;
+                }
+
+                // Verifica qual foi a escolha do usuário
+                switch (valorDigitado)
+                {
+                    case 1:
+                        // Realiza os comandos para gerar as chaves
+                        if (OpcaoGerarChaves(ref criptografer))
+                        {
+                            // Pergunta para o usuário se deseja continuar
+                            sair = MainClass.PerguntaDesejaContinuar(true);
+                        }
+
+                        break;
+
+                    case 2:
+
+                        // Realiza os comandos para a criptografia
+                        if (MainClass.OpcaoCriptografia(ref criptografer))
+                        {
+                            // Pergunta para o usuário se deseja continuar
+                            sair = MainClass.PerguntaDesejaContinuar(true);
+                        }
+
+                        break;
+
+                    case 3:
+
+                        // Realiza os comandos para a criptografia
+                        if (MainClass.OpcaoDescriptografia(ref criptografer))
+                        {
+                            // Pergunta para o usuário se deseja continuar
+                            sair = MainClass.PerguntaDesejaContinuar(true);
+                        }
+
+                        break;
+
+                    case 4:
+
+                        // Sai do programa
+                        sair = true;
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine(TEXTO_ENTRADA_INVALIDA);
+
+                        break;
+                }
+            }
+
+            Console.Write("Digite qualquer tecla para encerrar.");
+            Console.ReadKey();
+        }
+
+        public static bool ValidaUsuario(string login, string senha)
+        {
+            bool usuarioValido = true;
+
+            // Verifica se o login digitado existe (compara com o usuário do admin)
+            if (login != usuarioRoot.login)
+            {
+                // Caso seja usuário inválido, exibe mensagem e retorna a função
+                Console.WriteLine("Login inexistente.");
+                usuarioValido = false;
+            }
+            else
+            {
+                // Verifica se a senha está correta
+                if (senha != usuarioRoot.senha)
+                {
+                    // Caso seja senha inválida, exibe mensagem e retorna a função
+                    Console.WriteLine("Senha incorreta.");
+                    usuarioValido = false;
+                }
+            }
+
+            return usuarioValido;
+        }
+
+        public static bool OpcaoGerarChaves(ref RSACriptUnip criptografer)
+        {
+            int key_p = 0, key_q = 0, key_encrypt, key_descrypt, moduloN, totienteN;
+            bool encontrouPrimos = false;
+            Random rdn = new Random();
+
+            while (encontrouPrimos == false)
+            {
+                // Gera um valor aleatorio para P e Q tal que 101 <= p <= 997 e 101 <= q <= 997
+                key_p = rdn.Next(100, 997);
+                key_q = rdn.Next(1000);
+                key_q = rdn.Next(100, 997);
+
+                // Verifica se os números digitados são primos
+                if (!VerificaPrimo(key_p))
+                {
+                    encontrouPrimos = false;
+                }
+                else
+                {
+                    if (!VerificaPrimo(key_q))
+                    {
+                        encontrouPrimos = false;
+                    }
+                    else
+                    {
+                        encontrouPrimos = true;
+                    }
+                }
+            }
+
+            // Calcula a chave pública N e armazena no objeto
+            moduloN = criptografer.CalcularN(key_p, key_q);
+
+            // Calcula e retorna o totiente de N
+            totienteN = criptografer.CalcularTotienteN(key_p, key_q);
+
+            // Calcula a segunda chave púbica E e armazena no objeto
+            criptografer.CalcularE(totienteN);
+
+            // Pega a chave pública E(chave que utiliza para CRIPTOGRAFAR)
+            key_encrypt = criptografer.Key_Encrypt;
+
+            // Imprime para o usuário o módulo N
+            Console.WriteLine("\nGuarde o módulo N:");
+            Console.WriteLine(moduloN);
+
+            // Imprime para o usuário a chave de criptografia
+            Console.WriteLine("\nGuarde a chave de criptografia:");
+            Console.WriteLine(key_encrypt);
+
+            // Calcula e retorna a terceira chave privada D(chave que utiliza para DESCRIPTOGRAFAR)
+            key_descrypt = criptografer.CalcularInversoD(totienteN);
+
+            // Imprime para o usuário a chave de descriptografia
+            Console.WriteLine("\nGuarde a chave de descriptografia:");
+            Console.WriteLine(key_descrypt);
+
+            return true;
+        }
+
+        public static bool OpcaoCriptografia(ref RSACriptUnip criptografer)
+        {
+            int key_encrypt, moduloN;
+            string textoOriginal, textoCifrado = "";
+
+            // Solicita para o usuário digitar um texto
+            Console.WriteLine("\nDigite um texto, tudo em maiúsculo, para criptografar.");
+            Console.WriteLine("Obs: Ele deve conter no máximo 128 caracteres e não pode conter acento.");
+
+            // Pega o texto digitado
+            textoOriginal = Console.ReadLine();
+
+            // Verifica se o usuário digitou corretamente até 128 caracteres
+            if (textoOriginal.Length >= 128)
+            {
+                // Caso tenha digitado igual ou mais de 128, exibe mensagem de erro e reinicia
+                Console.WriteLine(TEXTO_MAIOR_QUE_128_RETORNA_PRINCIPAL);
+                return false;
+            }
+
+            // Solicita o módulo N
+            Console.WriteLine("\nDigite o módulo N:");
+
+            // Tenta converter moduloN
+            if (int.TryParse(Console.ReadLine(), out moduloN) == false && moduloN == 0)
+            {
+                // Caso tenha ocorrido um erro ao tentar converter o valor para inteiro, exibe uma mensagem de erro e retorna para a página principal
+                Console.WriteLine(TEXTO_ERRO_RETORNA_PRINCIPAL);
+                return false;
+            }
+
+            // Solicita a chave de criptografia
+            Console.WriteLine("\nDigite a chave de CRIPTOGRAFIA:");
+
+            // Tenta converter key_encrypt
+            if (int.TryParse(Console.ReadLine(), out key_encrypt) == false && key_encrypt == 0)
+            {
+                // Caso tenha ocorrido um erro ao tentar converter o valor para inteiro, exibe uma mensagem de erro e retorna para a página principal
+                Console.WriteLine(TEXTO_ERRO_RETORNA_PRINCIPAL);
+                return false;
+            }
+
+            // Realiza a criptografia do texto digitado fornecendo a chave de criptografia
+            textoCifrado = criptografer.Criptografar(textoOriginal, key_encrypt, moduloN);
+
+            // Imprime os controles de decisões
+            Console.WriteLine("\n\n=====================================");
+            Console.WriteLine("== TEXTO CRIPTOGRAFADO COM SUCESSO ==");
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Guarde o texto cifrado:\n");
+            Console.WriteLine(textoCifrado);
+            Console.WriteLine("\n\n");
+
+            return true;
+        }
+
+        public static bool OpcaoDescriptografia(ref RSACriptUnip criptografer)
+        {
+            int key_descrypt, moduloN;
+            string textoOriginal, textoCifrado = "";
+
+            // Solicita para o usuário digitar um texto
+            Console.WriteLine("\nDigite um texto para descriptografar:");
+
+            // Pega o texto digitado
+            textoCifrado = Console.ReadLine();
+
+            // Solicita o módulo N
+            Console.WriteLine("\nDigite o módulo N:");
+
+            // Tenta converter moduloN
+            if (int.TryParse(Console.ReadLine(), out moduloN) == false && moduloN == 0)
+            {
+                // Caso tenha ocorrido um erro ao tentar converter o valor para inteiro, exibe uma mensagem de erro e retorna para a página principal
+                Console.WriteLine(TEXTO_ERRO_RETORNA_PRINCIPAL);
+                return false;
+            }
+
+            // Solicita a chave de descriptografia
+            Console.WriteLine("\nDigite a chave de DESCRIPTOGRAFIA:");
+
+            // Tenta converter key_encrypt
+            if (int.TryParse(Console.ReadLine(), out key_descrypt) == false && key_descrypt == 0)
+            {
+                // Caso tenha ocorrido um erro ao tentar converter o valor para inteiro, exibe uma mensagem de erro e retorna para a página principal
+                Console.WriteLine(TEXTO_ERRO_RETORNA_PRINCIPAL);
+                return false;
+            }
+
+            // Realiza a criptografia do texto digitado fornecendo a chave de criptografia
+            textoOriginal = criptografer.Descriptografar(textoCifrado, key_descrypt, moduloN);
+
+            // Imprime os controles de decisões
+            Console.WriteLine("\n\n========================================");
+            Console.WriteLine("== TEXTO DESCRIPTOGRAFADO COM SUCESSO ==");
+            Console.WriteLine("========================================");
+            Console.WriteLine("Texto original:\n");
+            Console.WriteLine(textoOriginal);
+            Console.WriteLine("\n\n");
+
+            return true;
+        }
+
+        public static bool VerificaPrimo(int num)
+        {
+            bool primo = false;
+            int resto, totalDivisores = 0;
+
+            // loop para verificar se o número é primo
+            for (int i = 1; i <= num; i++)
+            {
+                // Calcula o resto da divisao do número pelo index
+                resto = num % i;
+
+                // Se possuir resto 0, aumenta o número de divisores (para ser primo precisa se dividr apenas por 1 e ele mesmo)
+                if (resto == 0)
+                {
+                    // Iteração de total de divisores
+                    totalDivisores++;
+                }
+            }
+
+            // Se possuir até 2 divisores (se n = 1 então possui apenas 1 divisor)
+            if (totalDivisores <= 2)
+            {
+                // Caso haja até 2 divisores, então seta que é primo
+                primo = true;
+            }
+
+            return primo;
+        }
+
+        public static bool PerguntaDesejaContinuar(bool repete)
+        {
+            char charResposta;
+            bool retorno = false;
+
+            // Se deu algum erro, repete
+            while (repete == true)
+            {
+                // Pergunta para o usuário se deseja continuar
+                Console.WriteLine("Deseja sair do programa? S/N");
+
+                // Tenta converter o charResposta
+                if (!char.TryParse(Console.ReadLine(), out charResposta))
+                {
+                    // Caso tenha ocorrido um erro ao tentar converter o valor para inteiro, exibe uma mensagem de erro e pergunta de novo
+                    Console.WriteLine("Valor digitado não corresponde com a resposta. Digitar somente 1 caractere!");
+                    repete = true;
+                }
+
+                switch (charResposta)
+                {
+                    case 'S':
+                    case 's':
+                        // Não manda repetir
+                        repete = false;
+
+                        // Retorna SIM
+                        retorno = true;
+
+                        break;
+
+                    case 'N':
+                    case 'n':
+                        // Não manda repetir
+                        repete = false;
+
+                        // retorna NÃO
+                        retorno = false;
+
+                        break;
+
+                    default:
+                        // Caso o usuário não tenha digitado nenhuma das opções
+                        Console.WriteLine("Valor digitado não corresponde com as opções. Digite S ou N!");
+                        repete = true;
+
+                        break;
+                }
+            }
+
+            return retorno;
         }
     }
 }
---
+++ RSACriptUnip.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace CriptografiaRSA
+{
+    public class RSACriptUnip
+    {
+            // Propriedade chave pública E
+        public int Key_Encrypt { get; set; }
+
+        // Primeira chave pública
+        public int CalcularN(int p, int q)
+        {
+            int n = 0;
+
+            // Verifica se possuímos p e q recebidos
+            if (p != 0 && q != 0)
+            {
+                // Multiplica os dois números primos para gerar a chave pública
+                n = p * q;
+            }
+
+            // Armazena o valor da chave pública N
+            return n;
+        }
+
+        public int CalcularTotienteN(int p, int q)
+        {
+            int totienteN = 0;
+
+            // Verifica se possuímos p e q recebidos
+            if (p != 0 && q != 0)
+            {
+                // Calcula a função totiente de n
+                totienteN = (p - 1) * (q - 1);
+            }
+
+            return totienteN;
+        }
+
+        // Segunda chave pública
+        public void CalcularE(int totienteN)
+        {
+            int e = 0;
+            List<int> divisoresE = new List<int>();
+            List<int> divisoresTotienteN = new List<int>();
+            bool podeSer = false;
+            Random numRandomico = new Random();
+            int countMenor = 0;
+            int countMaior = 0;
+            int numMenorAux = 0;
+            int numMaiorAux = 0;
+
+            // Verifica se possuímos o totiente de N
+            if (totienteN != 0)
+            {
+                // Enquanto o número escolhido para E não puder ser utilizado, recalcula
+                while (podeSer == false)
+                {
+                    // Escolhe um número aleatório que satisfaça 1 > e > totienteN
+                    e = numRandomico.Next(2, totienteN);
+
+                    // Pega todos os divisores de e
+                    divisoresE = RetornaDivisores(e);
+
+                    // Pega todos os divisores de totiente de N
+                    divisoresTotienteN = RetornaDivisores(totienteN);
+
+                    // Verifica qual dos 2 arrays possuem menores elementos para poder utilizar como base para o loop
+                    countMenor = (divisoresE.Count > divisoresTotienteN.Count ? divisoresTotienteN.Count : divisoresE.Count);
+
+                    // Pega qual possui maiores elementos
+                    countMaior = (divisoresE.Count > divisoresTotienteN.Count ? divisoresE.Count : divisoresTotienteN.Count);
+
+                    // Loop para verificar se E e totienteN possuem divisores comuns
+                    for (int i = 0; i < countMenor; i++)
+                    {
+                        for (int j = 0; j < countMaior; j++)
+                        {
+                            // Redescobre qual array possui menores elementos
+                            if (countMenor == divisoresE.Count)
+                            {
+                                // Os divisores de E possuem os menores elementos
+                                // Armazena o valor do primeiro index i e o numero do laço atual j
+                                numMenorAux = divisoresE[i];
+                                numMaiorAux = divisoresTotienteN[j];
+                            }
+                            else
+                            {
+                                // Os divisores de totienteN possuem os menores elementos
+                                // Armazena o valor do primeiro index i e o numero do laço atual j
+                                numMenorAux = divisoresTotienteN[i];
+                                numMaiorAux = divisoresE[j];
+                            }
+
+                            // 'e' e 'totienteN' possuem divisores em comum? (Com excessão de 1)
+                            if (numMenorAux == numMaiorAux && numMenorAux != 1)
+                            {
+                                // Caso possuam, seta que o valor de 'e' criado não pode ser utilizado e refaz o while (finaliza o loop menor para que possa finalizar o loop maior)
+                                podeSer = false;
+                                break;
+                            }
+                            else
+                            {
+                                // Até que o loop completo termine, ainda não houve divisores em comum. Ou seja, até agora o valor de 'e' pode ser utilizado
+                                podeSer = true;
+                            }
+                        }
+
+                        // Se encontramos divisores em comum, 'e' atual não pode ser utilizado
+                        if (podeSer == false)
+                        {
+                            // Finaliza o loop maior
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Armazena o valor da chave pública E
+            Key_Encrypt = e;
+        }
+
+        // Terceira chave privada (p e q são as duas primeiras)
+        public int CalcularInversoD(int totienteN)
+        {
+            int d = 0;
+
+            // Calcula o inverso multiplicativo de e:totienteN.
+            for (d = 1; d <= totienteN; d++)
+            {
+                // 'd' satisfaz a seguinte condição? (d * e) % totienteN == 1
+                if ((d * Key_Encrypt) % totienteN == 1)
+                {
+                    // Então 'd' é o inverso multiplicativo e, portanto, não precisa mais continuar a busca. Finaliza o loop.
+                    break;
+                }
+            }
+
+            return d;
+        }
+
+        static List<int> RetornaDivisores(int numero)
+        {
+            List<int> divisoresNumero = new List<int>();
+            int resto = 0;
+
+            // Loop para calcular todos os divisores do numero
+            for (int i = 1; i <= numero; i++)
+            {
+                // Pega o resto da divisão
+                resto = numero % i;
+
+                // Se i é um divisor
+                if (resto == 0)
+                {
+                    // Adiciona no array de divisores do numero
+                    divisoresNumero.Add(i);
+                }
+            }
+
+            return divisoresNumero;
+        }
+
+        public string Criptografar(string textoSimples, int key_Encrypt, int moduloN)
+        {
+            char[] caracteres;
+            int intAux;
+            string stringCifraCompleta = "";
+            BigInteger numeroNovo;
+
+            // Passa o texto simples para um array contendo cada um dos caracteres
+            caracteres = textoSimples.ToCharArray();
+
+            // Loop por todos os caracteres do texto simples
+            for (int i = 0; i < caracteres.Length; i++)
+            {
+                // Converte a letra para ASCII
+                intAux = Convert.ToInt32(caracteres[i]);
+
+                // Realiza a conta: (intAux ^ key_Encrypt) % moduloN
+                numeroNovo = BigInteger.ModPow(intAux, key_Encrypt, moduloN);
+
+                // Quando for o primeiro número
+                if (stringCifraCompleta == "")
+                {
+                    // Apenas adiciona o numero novo
+                    stringCifraCompleta += numeroNovo.ToString();
+                }
+                else
+                {
+                    // Coloca um caractere de separacao e adiciona o numero novo
+                    stringCifraCompleta += "-" + numeroNovo.ToString();
+                }
+            }
+
+            // Retorna o texto cifrado
+            return stringCifraCompleta;
+        }
+
+        public string Descriptografar(string textoCifrado, int key_Descrypt, int moduloN)
+        {
+            string[] arrayTextoCifradoSeparado;
+            BigInteger numCifradoAux;
+            BigInteger numOriginal;
+            string textoOriginal = "";
+
+            // Realiza um split do texto cifrado, eliminando os separador "-"
+            arrayTextoCifradoSeparado = textoCifrado.Split(new[] { '-' });
+
+            // Loop para percorrer todas as cifras
+            for (int i = 0; i < arrayTextoCifradoSeparado.Length; i++)
+            {
+                // Pega o número armazenado na string auxiliar
+                numCifradoAux = BigInteger.Parse(arrayTextoCifradoSeparado[i]);
+
+                // Realiza a conta: (intAux ^ key_Descrypt) % moduloN
+                numOriginal = BigInteger.ModPow(numCifradoAux, key_Descrypt, moduloN);
+
+                // Converte de número para caractere pela tabela ASCII
+                textoOriginal += Convert.ToChar((int)numOriginal);
+            }
+
+            // Retorna o texto original
+            return textoOriginal;
+        }
+    }
+}
